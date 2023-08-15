1a44602bf5427906b22e9d2614f9c421269ffe11
Christian <chca.parayno.28@gmail.com>
2023-08-15 21:50:25 +0800

    change bg-color

--- app.js
+++ app.js
@@ -7,6 +7,15 @@ hamburger.addEventListener('click', () => {
     mobile_menu.classList.toggle('active');
 })
 
+document.addEventListener('scroll', ()=>{
+    var scroll_position = window.scrollY;
+    if(scroll_position > 300) {
+        header.style.backgroundColor = '#29323c';
+    } else {
+        header.style.backgroundColor = 'transparent';
+    }
+})
+
 menu_item.forEach((item) => {
     item.addEventListener('click', () => {
         hamburger.classList.toggle('active');
